3a95f8ab39d8b82a415da240e2cc3ce0d9401f1b
Esteban Saiz <estebansa@blinkbox.com>
2014-02-28 13:12:25 +0000

    Some fixes to pass jshint

--- app/demo/scripts/script.js
+++ app/demo/scripts/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* jshint devel:true */
-/* globals READER */
+/* globals $, READER */
 
 var app = angular.module('app', []);
 
@@ -19,16 +19,9 @@ app.controller('Reader_controller', function ($scope, $exceptionHandler, Book) {
         $('#log div').prepend('<p>' + JSON.stringify(e) + '</p>');
       }
 
-      // Validate ISBN
-      function isValidISBN (a) {
-        for (var b = /\d/g, c = 0, d, e = 25; d = b.exec(a); e -= 2)
-          c += e % 4 * d;
-        return !(~e | c % 10);
-      }
-
       // ISBN
       $scope.$watch('isbn.value', function(val){
-        if (val && isValidISBN(val)) {
+        if (val) {
           Book.get({}, PRODUCTION_API + val).then(function (response) {
             if (response.data.code === 200 || !response.data.code) {
               if (response.data.links && response.data.links.length > 0) {
@@ -42,20 +35,11 @@ app.controller('Reader_controller', function ($scope, $exceptionHandler, Book) {
                     a.href = link.href;
                     var samplePath = a.pathname.indexOf('/') === 0 ? a.pathname : '/'+a.pathname; // add leading slash if not present in pathname (IE bug)
                     URL = '//'+ a.hostname+'/params;v=0'+ samplePath;     // prepend the mandatory matrix params
-                    READER.init({container: '#reader_container', width: 400, height: 400, padding: 0, url: URL, bookmarks: [], listener: eventHandler, preferences: { margin: [9.800000,11.000000,6.500000,11.000000], lineHeight: 1.583000, fontSize: 1.497656, textAlign: 'left', fontFamily: 'Helvetica', theme: 'light'}});
+                    READER.init({container: '#reader_container', width: 400, height: 400, padding: 0, url: URL, bookmarks: [], listener: eventHandler, preferences: { margin: [9.800000,11.000000,6.500000,11.000000], textAlign: 'left', fontFamily: 'Helvetica', theme: 'light'}});
 
                     $scope.$watch('lineHeight.value', READER.setLineHeight);
                     $scope.$watch('fontSize.value', READER.setFontSize);
 
-                    // Width and height
-                    $scope.$watch('width.value', function(val){
-                      $scope.dimensions.width = val;
-                      READER.resizeContainer($scope.dimensions);
-                    });
-                    $scope.$watch('height.value', function(val){
-                      $scope.dimensions.height = val;
-                      READER.resizeContainer($scope.dimensions);
-                    });
                     break;
                   }
                 }
@@ -80,14 +64,26 @@ app.controller('Reader_controller', function ($scope, $exceptionHandler, Book) {
         padding: 0
       }, $scope.dimensions);
 
-
+      // Width and height
+      $scope.$watch('width.value', function(val){
+        if (val >= 0) {
+          $scope.dimensions.width = val;
+          READER.resizeContainer($scope.dimensions);
+        }
+      });
+      $scope.$watch('height.value', function(val){
+        if (val >= 0) {
+          $scope.dimensions.height = val;
+          READER.resizeContainer($scope.dimensions);
+        }
+      });
 
       // Line height increase/decrease
       $scope.lineHeight = {
         min: 1.1,
         max: 20,
         step: 0.01,
-        value: 1.583000
+        value: 1.2
       };
 
       // Font-size increase/decrease
@@ -95,7 +91,7 @@ app.controller('Reader_controller', function ($scope, $exceptionHandler, Book) {
         min: 0.5,
         max: 15,
         step: 0.1,
-        value: 1.497656
+        value: 1
       };
 
 
@@ -134,12 +130,12 @@ app.controller('Reader_controller', function ($scope, $exceptionHandler, Book) {
 app.factory('Book', function($http, $q) {
       var _call = function(args, uri){
         var deferred = $q.defer();
-          var _defaults = {
-            url: uri,
-            method: 'GET',
-            params: args
-          };
-          $http(_defaults).then(deferred.resolve, deferred.reject);
+        var _defaults = {
+          url: uri,
+          method: 'GET',
+          params: args
+        };
+        $http(_defaults).then(deferred.resolve, deferred.reject);
         return deferred.promise;
       };
 
