b05b0d9c7e05f3443d4a6da79ff851a93c2c0b4e
Vilmos Ioo <vilmosi@blinkbox.com>
2014-03-05 16:39:14 +0000

    Updated Demo UI

--- app/demo/scripts/script.js
+++ app/demo/scripts/script.js
@@ -9,131 +9,135 @@ var app = angular.module('app', []);
 
 app.controller('Reader_controller', function ($scope, $exceptionHandler, Book) {
 
-      // Use '/books/' + isbn + '/' if you want to check the books in your localhost (you would need the books from the share drive  /Documents/ePubs/Test-Books-book-info-v1.2.zip
-      var PRODUCTION_API = 'https://api.blinkboxbooks.com/service/catalogue/books/';
+	// Use '/books/' + isbn + '/' if you want to check the books in your localhost (you would need the books from the share drive  /Documents/ePubs/Test-Books-book-info-v1.2.zip
+	var PRODUCTION_API = 'https://api.blinkboxbooks.com/service/catalogue/books/';
 
-      // Reader event handler
-      function eventHandler(e){
-        console.log(e);
-        $('#log div').prepend('<p>' + JSON.stringify(e) + '</p>');
-      }
+	// Reader event handler
+	function _log(e){
+		$('#log .panel-body').prepend('<p>' + JSON.stringify(e) + '</p>');
+	}
 
-      // ISBN
-      $scope.$watch('isbn.value', function(val){
-        if (val) {
-          Book.get(PRODUCTION_API + val).then(function (url) {
-                // Call the Reader
-                READER.init({container: '#reader_container', width: 400, height: 400, padding: 0, url: url, bookmarks: [], listener: eventHandler, preferences: { margin: [9.800000,11.000000,6.500000,11.000000], textAlign: 'left', fontFamily: 'Helvetica', theme: 'light'}});
+	// the current book loaded
+	$scope.book = {
+		isbn: '',
+		url: ''
+	};
 
-                $scope.$watch('lineHeight.value', READER.setLineHeight);
-                $scope.$watch('fontSize.value', READER.setFontSize);
-                $scope.$watch('font', function(val){
-                  READER.setFontFamily(val);
-                });
+	$scope.layout = {
+		width: 600,
+		height: 800,
+		columns: 1,
+		padding:0
+	};
 
-              },
-          function (error) {
-            $exceptionHandler({message: error.status, stack: error.config}, error);
-          }
-          );
-        }
-      });
+	$scope.preferences = {
+		margin: 'medium',
+		textAlign: 'left',
+		fontFamily: 'Helvetica',
+		theme: 'light',
+		lineHeight: 1.2,
+		fontSize: 1
+	};
 
-      $scope.dimensions = angular.extend({
-        width: '500',
-        height: '600',
-        columns: 1,
-        padding: 0
-      }, $scope.dimensions);
+	// Line height increase/decrease
+	$scope.formSettings = {
+		lineHeight: {
+			min: 1.1,
+			max: 20,
+			step: 0.01
+		},
+		fontSize: {
+			min: 0.5,
+			max: 15,
+			step: 0.1
+		},
+		fonts: ['Helvetica', 'Times new Roman', 'Courier New', 'Arial'],
+		alignment: ['left', 'justify'],
+		margins: ['min', 'medium', 'max'],
+		themes: ['light', 'transparent','dark', 'sepia']
+	};
 
-      // Width and height
-      $scope.$watch('width.value', function(val){
-        if (val >= 0) {
-          $scope.dimensions.width = val;
-          READER.resizeContainer($scope.dimensions);
-        }
-      });
-      $scope.$watch('height.value', function(val){
-        if (val >= 0) {
-          $scope.dimensions.height = val;
-          READER.resizeContainer($scope.dimensions);
-        }
-      });
+	$scope.handlers = {
+		prev: function(){
+			READER.prev();
+		},
+		next: function(){
+			READER.next();
+		},
+		cfi: function(){
+			try{
+				_log(decodeURIComponent(READER.getCFI()));
+			} catch(e){
+				_log(e);
+			}
+		}
+	};
 
-      // Line height increase/decrease
-      $scope.lineHeight = {
-        min: 1.1,
-        max: 20,
-        step: 0.01,
-        value: 1.2
-      };
+	$scope.$watch('book.isbn', function(val){
+		if (val) {
+			// ng-pattern will validate the input for us, we can assume the ISBN is valid (the book may not exist though)
+			Book.get(PRODUCTION_API + val).then(function (url) {
+				$scope.book.url = url;
+			},
+			function (error) {
+				$exceptionHandler({message: error.status, stack: error.config}, error);
+			});
+		}
+	});
 
-      // Font-size increase/decrease
-      $scope.fontSize = {
-        min: 0.5,
-        max: 15,
-        step: 0.1,
-        value: 1
-      };
+	var _isWatching = false; // flag that tells us if we enabled the watches
+	$scope.$watch('book.url', function(val){
+		if(val){
+			// Call the Reader
+			READER.init({
+				container: '#reader_container',
+				width: $scope.layout.width,
+				height: $scope.layout.height,
+				padding: $scope.layout.padding,
+				columns: $scope.layout.columns,
+				url: val,
+				bookmarks: [],
+				listener: _log,
+				preferences: $scope.preferences
+			});
 
-
-      // font family
-      $scope.fonts = ['Helvetica', 'Times new Roman', 'Courier New', 'Arial'];
-      $scope.font = $scope.fonts[0];
-
-
-      $scope.alignment = ['left', 'justify'];
-      $scope.align = $scope.alignment[0];
-      $scope.$watch('align', function(val){
-        READER.setTextAlign(val);
-      });
-
-      $scope.setTheme = READER.setTheme;
-
-      // Margins
-      $scope.setMargin = READER.setMargin;
-
-      // CFI
-      $scope.getCFI = function(){
-        console.log('Current CFI', decodeURIComponent(READER.getCFI()));
-      };
-
-      // Next/previous chapter navigation.
-      $scope.next = function () {
-        READER.next();
-      };
-
-      $scope.previous = function () {
-        READER.prev();
-      };
-    });
+			// watch for new watches
+			if(!_isWatching){
+				console.log('Watching');
+				$scope.$watch('preferences', READER.setPreferences, true);
+				$scope.$watch('layout', READER.resizeContainer, true);
+				_isWatching = true;
+			}
+		}
+	});
+});
 
 // Service to access to the API of Catalogue
 app.factory('Book', function($http) {
-      return {
-        get: function(uri){
-          var _defaults = {
-            url: uri,
-            method: 'GET'
-          };
-          return $http(_defaults).then(function (response) {
-                if (response.data.links && response.data.links.length > 0) {
-                  var linkIdx, l;
-                  for (linkIdx = 0, l = response.data.links.length; linkIdx !== l; linkIdx += 1) {
-                    var link = response.data.links[linkIdx];
-                    if (link.title === 'Sample' &&  link.href!=='') {
-                      // this is a slightly obscure, but simple method of extracting a path from an href
-                      // ref: http://stackoverflow.com/questions/8498592/extract-root-domain-name-from-string
-                      var a = document.createElement('a');
-                      a.href = link.href;
-                      var samplePath = a.pathname.indexOf('/') === 0 ? a.pathname : '/'+a.pathname; // add leading slash if not present in pathname (IE bug)
-                      return '//'+ a.hostname+'/params;v=0'+ samplePath;     // prepend the mandatory matrix params
-                    }
-                  }
-                }
-              }
-          );
-        }
-      };
-    }
+		return {
+			get: function(uri){
+				var _defaults = {
+					url: uri,
+					method: 'GET'
+				};
+				return $http(_defaults).then(function (response) {
+						if (response.data.links && response.data.links.length > 0) {
+							var linkIdx, l;
+							for (linkIdx = 0, l = response.data.links.length; linkIdx !== l; linkIdx += 1) {
+								var link = response.data.links[linkIdx];
+								if (link.title === 'Sample' &&  link.href!=='') {
+									// this is a slightly obscure, but simple method of extracting a path from an href
+									// ref: http://stackoverflow.com/questions/8498592/extract-root-domain-name-from-string
+									var a = document.createElement('a');
+									a.href = link.href;
+									var samplePath = a.pathname.indexOf('/') === 0 ? a.pathname : '/'+a.pathname; // add leading slash if not present in pathname (IE bug)
+									return '//'+ a.hostname+'/params;v=0'+ samplePath;     // prepend the mandatory matrix params
+								}
+							}
+						}
+					}
+				);
+			}
+		};
+	}
 );
