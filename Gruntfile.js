2a01b8f77f4d2fd9cc523b892018b6dc0f3c37c2
Vilmos Ioo <vilmosi@blinkbox.com>
2014-03-05 13:45:08 +0000

    Updated Jenkins scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -357,6 +357,19 @@ module.exports = function (grunt) {
 					output: '<%= yeoman.dist %>/docs/'
 				}
 			}
+		},
+		// this task is used to manage the package.json version
+		bump: {
+			options: {
+				files: ['package.json'],
+				updateConfigs: [],
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['package.json'], // '-a' for all files
+				createTag: false,
+				push: false,
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+			}
 		}
 	});
 
