0788ddabf30503d4f48883423c07df4f79c88d20
Vilmos Ioo <vilmosi@blinkbox.com>
2014-03-06 10:14:48 +0000

    Demo update. Added angular-route to listen to isbn in the route param

--- app/demo/scripts/script.js
+++ app/demo/scripts/script.js
@@ -1,119 +1,127 @@
 'use strict';
 
-/* jshint devel:true */
-/* globals $, READER */
-
-var app = angular.module('app', []);
-
 // Controller for configure the Reader and init it.
+angular.module('app', ['ngRoute'])
+	.config(function($routeProvider, $locationProvider) {
+		$locationProvider.html5Mode(false).hashPrefix('!');
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'Reader_controller'
+			})
+			.when('/:isbn', {
+				templateUrl: 'views/main.html',
+				controller: 'Reader_controller'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	})
+	.controller('Reader_controller', function ($scope, $routeParams, $exceptionHandler, Book) {
 
-app.controller('Reader_controller', function ($scope, $exceptionHandler, Book) {
+		// Use '/books/' + isbn + '/' if you want to check the books in your localhost (you would need the books from the share drive  /Documents/ePubs/Test-Books-book-info-v1.2.zip
+		var PRODUCTION_API = 'https://api.blinkboxbooks.com/service/catalogue/books/';
 
-	// Use '/books/' + isbn + '/' if you want to check the books in your localhost (you would need the books from the share drive  /Documents/ePubs/Test-Books-book-info-v1.2.zip
-	var PRODUCTION_API = 'https://api.blinkboxbooks.com/service/catalogue/books/';
-
-	// Reader event handler
-	function _log(e){
-		$('#log .panel-body').prepend('<p>' + JSON.stringify(e) + '</p>');
-	}
-
-	// the current book loaded
-	$scope.book = {
-		isbn: '',
-		url: ''
-	};
-
-	$scope.layout = {
-		width: 600,
-		height: 800,
-		columns: 1,
-		padding:0
-	};
-
-	$scope.preferences = {
-		margin: 'medium',
-		textAlign: 'left',
-		fontFamily: 'Helvetica',
-		theme: 'light',
-		lineHeight: 1.2,
-		fontSize: 1
-	};
-
-	// Line height increase/decrease
-	$scope.formSettings = {
-		lineHeight: {
-			min: 1.1,
-			max: 20,
-			step: 0.01
-		},
-		fontSize: {
-			min: 0.5,
-			max: 15,
-			step: 0.1
-		},
-		fonts: ['Helvetica', 'Times new Roman', 'Courier New', 'Arial'],
-		alignment: ['left', 'justify'],
-		margins: ['min', 'medium', 'max'],
-		themes: ['light', 'transparent','dark', 'sepia']
-	};
-
-	$scope.handlers = {
-		prev: function(){
-			READER.prev();
-		},
-		next: function(){
-			READER.next();
-		},
-		cfi: function(){
-			try{
-				_log(decodeURIComponent(READER.getCFI()));
-			} catch(e){
-				_log(e);
-			}
+		// Reader event handler
+		function _log(e){
+			$('#log .panel-body').prepend('<p>' + JSON.stringify(e) + '</p>');
 		}
-	};
 
-	$scope.$watch('book.isbn', function(val){
-		if (val) {
-			// ng-pattern will validate the input for us, we can assume the ISBN is valid (the book may not exist though)
-			Book.get(PRODUCTION_API + val).then(function (url) {
-				$scope.book.url = url;
+		// the current book loaded
+		$scope.book = {
+			isbn: $routeParams.isbn || '',
+			url: ''
+		};
+
+		$scope.layout = {
+			width: 600,
+			height: 800,
+			columns: 1,
+			padding:0
+		};
+
+		$scope.preferences = {
+			margin: 'medium',
+			textAlign: 'left',
+			fontFamily: 'Helvetica',
+			theme: 'light',
+			lineHeight: 1.2,
+			fontSize: 1
+		};
+
+		// Line height increase/decrease
+		$scope.formSettings = {
+			lineHeight: {
+				min: 1.1,
+				max: 20,
+				step: 0.01
 			},
-			function (error) {
-				$exceptionHandler({message: error.status, stack: error.config}, error);
-			});
-		}
-	});
+			fontSize: {
+				min: 0.5,
+				max: 15,
+				step: 0.1
+			},
+			fonts: ['Helvetica', 'Times new Roman', 'Courier New', 'Arial'],
+			alignment: ['left', 'justify'],
+			margins: ['min', 'medium', 'max'],
+			themes: ['light', 'transparent','dark', 'sepia']
+		};
 
-	var _isWatching = false; // flag that tells us if we enabled the watches
-	$scope.$watch('book.url', function(val){
-		if(val){
-			// Call the Reader
-			READER.init({
-				container: '#reader_container',
-				width: $scope.layout.width,
-				height: $scope.layout.height,
-				padding: $scope.layout.padding,
-				columns: $scope.layout.columns,
-				url: val,
-				bookmarks: [],
-				listener: _log,
-				preferences: $scope.preferences
-			});
-
-			// watch for new watches
-			if(!_isWatching){
-				console.log('Watching');
-				$scope.$watch('preferences', READER.setPreferences, true);
-				$scope.$watch('layout', READER.resizeContainer, true);
-				_isWatching = true;
+		$scope.handlers = {
+			prev: function(){
+				READER.prev();
+			},
+			next: function(){
+				READER.next();
+			},
+			cfi: function(){
+				try{
+					_log(decodeURIComponent(READER.getCFI()));
+				} catch(e){
+					_log(e);
+				}
 			}
-		}
-	});
-});
+		};
 
-// Service to access to the API of Catalogue
-app.factory('Book', function($http) {
+		$scope.$watch('book.isbn', function(val){
+			if (val) {
+				// ng-pattern will validate the input for us, we can assume the ISBN is valid (the book may not exist though)
+				Book.get(PRODUCTION_API + val).then(function (url) {
+					$scope.book.url = url;
+				},
+				function (error) {
+					$exceptionHandler({message: error.status, stack: error.config}, error);
+				});
+			}
+		});
+
+		var _isWatching = false; // flag that tells us if we enabled the watches
+		$scope.$watch('book.url', function(val){
+			if(val){
+				// Call the Reader
+				READER.init({
+					container: '#reader_container',
+					width: $scope.layout.width,
+					height: $scope.layout.height,
+					padding: $scope.layout.padding,
+					columns: $scope.layout.columns,
+					url: val,
+					bookmarks: [],
+					listener: _log,
+					preferences: $scope.preferences
+				});
+
+				// watch for new watches
+				if(!_isWatching){
+					$scope.$watch('preferences', READER.setPreferences, true);
+					$scope.$watch('layout', READER.resizeContainer, true);
+					_isWatching = true;
+				}
+			}
+		});
+	})
+	// Service to access to the API of Catalogue
+	.factory('Book', function($http) {
 		return {
 			get: function(uri){
 				var _defaults = {
