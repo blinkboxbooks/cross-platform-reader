44576f10623514858a7fbf39a59028f6ceea53e8
Vilmos Ioo <vilmosi@blinkbox.com>
2014-03-24 09:49:11 +0000

    Installed vagrant

---
+++ vagrant-conf/scripts/dist-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# build
+vagrant ssh-config | ssh -F /dev/stdin default 'cd /vagrant;npm install;bower install;grunt;'
+
+# use nginx 'dist' config and restart nginx
+vagrant ssh-config | ssh -F /dev/stdin default 'sudo cp /vagrant/vagrant-conf/nginx/ci-conf/nginx.conf /usr/local/nginx/conf;sudo service nginx reload'
