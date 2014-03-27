126127405166a6d62c2f2a736a9a2d3ede1da6e0
Vilmos Ioo <vilmosi@blinkbox.com>
2014-03-27 09:50:41 +0000

    Added line height tests

--- test/reader/spec/formatting.js
+++ test/reader/spec/formatting.js
@@ -2,7 +2,15 @@
 
 describe('Formatting', function() {
 
-	var testBookUrl = '/base/app/books/9780007441235', readerID = '#reader_container', fontSizeUnit = 18, defaultFontSize = 1,
+	var testBookUrl = '/base/app/books/9780007441235', readerID = '#reader_container',
+		fontSize = {
+			unit: 18,
+			default: 1
+		},
+		lineHeight = {
+			unit: 16,
+			default: 1.2
+		},
 		flags = {
 			isLoaded: false,
 			hasErrors: false
@@ -43,18 +51,18 @@ describe('Formatting', function() {
 			runs(function(){
 				var $contents = $(readerID).find('span, p, em, div, strong, a');
 				expect($contents).toHaveCss({
-					fontSize: (defaultFontSize * fontSizeUnit) + 'px'
+					fontSize: (fontSize.default * fontSize.unit) + 'px'
 				});
 			});
 
 		});
 
 		it('should initialise the reader with the given font size', function() {
-			var fontSize = 2;
+			var value = 2;
 
 			READER.init($.extend({
 				preferences: {
-					fontSize: fontSize
+					fontSize: value
 				}
 			}, defaultArgs));
 
@@ -65,13 +73,13 @@ describe('Formatting', function() {
 			runs(function(){
 				var $contents = $(readerID).find('span, p, em, div, strong, a');
 				expect($contents).toHaveCss({
-					fontSize: (fontSizeUnit * fontSize) + 'px'
+					fontSize: (fontSize.unit * value) + 'px'
 				});
 			});
 		});
 
 		it('should apply font-size', function(){
-			var fontSize = 1.5, fontSize2 = 2.5;
+			var value = 1.5, value2 = 2.5;
 
 			READER.init($.extend({}, defaultArgs));
 
@@ -82,16 +90,16 @@ describe('Formatting', function() {
 			runs(function(){
 				var $contents = $(readerID).find('span, p, em, div, strong, a');
 
-				READER.setFontSize(fontSize);
+				READER.setFontSize(value);
 				expect($contents).toHaveCss({
-					fontSize: (fontSizeUnit * fontSize) + 'px'
+					fontSize: (fontSize.unit * value) + 'px'
 				});
 
 				READER.setPreferences({
-					fontSize: fontSize2
+					fontSize: value2
 				});
 				expect($contents).toHaveCss({
-					fontSize: (fontSizeUnit * fontSize2) + 'px'
+					fontSize: (fontSize.unit * value2) + 'px'
 				});
 			});
 		});
@@ -117,7 +125,7 @@ describe('Formatting', function() {
 				READER.increaseFontSize();
 				READER.increaseFontSize();
 				expect($contents).toHaveCss({
-					fontSize: Math.round((defaultFontSize + 2 * step) * fontSizeUnit) + 'px'
+					fontSize: Math.round((fontSize.default + 2 * step) * fontSize.unit) + 'px'
 				});
 
 			});
@@ -143,7 +151,137 @@ describe('Formatting', function() {
 				// calling increase font size twice
 				READER.decreaseFontSize();
 				expect($contents).toHaveCss({
-					fontSize: Math.round((defaultFontSize - step) * fontSizeUnit) + 'px'
+					fontSize: Math.round((fontSize.default - step) * fontSize.unit) + 'px'
+				});
+
+			});
+		});
+
+		it('should handle invalid arguments', function(){
+
+		});
+	});
+
+	describe('Line height', function() {
+
+		it('should initialise the reader with the default line height', function() {
+			READER.init($.extend({}, defaultArgs));
+
+			waitsFor(function(){
+				return flags.isLoaded;
+			}, 'the reader to load the book', 10000);
+
+			runs(function(){
+				var $contents = $(readerID).find('span, p, em, div, strong, a');
+				var fontSize = parseInt($contents.css('fontSize'));
+				expect($contents).toHaveCss({
+					// line height appears to be computed as the floor value of the actual value
+					lineHeight: Math.floor(lineHeight.default * fontSize) + 'px'
+				});
+			});
+
+		});
+
+		it('should initialise the reader with the given line height', function() {
+			var value = 2;
+
+			READER.init($.extend({
+				preferences: {
+					lineHeight: value
+				}
+			}, defaultArgs));
+
+			waitsFor(function(){
+				return flags.isLoaded;
+			}, 'the reader to load the book', 10000);
+
+			runs(function(){
+				var $contents = $(readerID).find('span, p, em, div, strong, a');
+				var fontSize = parseInt($contents.css('fontSize'));
+				expect($contents).toHaveCss({
+					lineHeight: (fontSize * value) + 'px'
+				});
+			});
+		});
+
+		it('should apply line height', function(){
+			var value = 1.5, value2 = 2.5;
+
+			READER.init($.extend({}, defaultArgs));
+
+			waitsFor(function(){
+				return flags.isLoaded;
+			}, 'the reader to load the book', 10000);
+
+			runs(function(){
+				var $contents = $(readerID).find('span, p, em, div, strong, a');
+
+				READER.setLineHeight(value);
+				fontSize = parseInt($contents.css('fontSize'));
+				expect($contents).toHaveCss({
+					lineHeight: (fontSize * value) + 'px'
+				});
+
+				READER.setPreferences({
+					lineHeight: value2
+				});
+				fontSize = parseInt($contents.css('fontSize'));
+				expect($contents).toHaveCss({
+					lineHeight: (fontSize * value2) + 'px'
+				});
+			});
+		});
+
+		it('should increase line height', function(){
+
+			var step = 0.1;
+
+			READER.init($.extend({
+				preferences: {
+					lineHeight: 2
+				}
+			}, defaultArgs));
+
+			waitsFor(function(){
+				return flags.isLoaded;
+			}, 'the reader to load the book', 10000);
+
+			runs(function(){
+				var $contents = $(readerID).find('span, p, em, div, strong, a');
+				fontSize = parseInt($contents.css('fontSize'));
+
+				// calling increase line height twice
+				READER.increaseLineHeight();
+				READER.increaseLineHeight();
+				expect($contents).toHaveCss({
+					lineHeight: Math.round((2 + 2 * step) * fontSize) + 'px'
+				});
+
+			});
+		});
+
+		it('should decrease line height', function(){
+
+			var step = 0.1;
+
+			READER.init($.extend({
+				preferences: {
+					lineHeight: 2
+				}
+			}, defaultArgs));
+
+			waitsFor(function(){
+				return flags.isLoaded;
+			}, 'the reader to load the book', 10000);
+
+			runs(function(){
+				var $contents = $(readerID).find('span, p, em, div, strong, a');
+				fontSize = parseInt($contents.css('fontSize'));
+
+				// calling increase font size twice
+				READER.decreaseLineHeight();
+				expect($contents).toHaveCss({
+					lineHeight: Math.round((2 - step) * fontSize) + 'px'
 				});
 
 			});
