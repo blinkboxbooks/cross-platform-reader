0100aa5097bb4a9a763f977d6b7dd2b4793510e2
ci-user <ci-user@mobcast.co.uk>
2014-04-07 10:23:56 +0000

    Added build v0.1.27-73 to gh-pages

--- latest/reader/27084f92.reader.js
+++ latest/reader/27084f92.reader.js
@@ -3986,7 +3986,7 @@ var Reader = (function (r) {
 			r.Bugsense = new Bugsense({
 				apiKey: 'f38df951',
 				appName: 'CPR',
-				appversion: '0.1.27-72'
+				appversion: '0.1.27-73'
 			});
 			// Setup error handler
 			window.onerror = function (message, url, line) {
@@ -4524,7 +4524,7 @@ var Reader = (function (r) {
 		STATUS: {
 			'code': 7,
 			'message': 'Reader has updated its status.',
-			'version': '0.1.27-72'
+			'version': '0.1.27-73'
 		},
 		START_OF_BOOK : {
 			code: 8,
