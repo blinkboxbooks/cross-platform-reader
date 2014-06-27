77f65e9f836fea64a5193b90e9aabf98d04faf1c
Vilmos Ioo <vilmosi@blinkbox.com>
2014-06-27 15:21:00 +0100

    Updated TC config

--- config/build.sh
+++ config/build.sh
@@ -10,16 +10,6 @@ echo "installing dependencies..."
 npm install --quiet
 bower install
 
-# We need to switch to the master branch before making any changes that we want to commit
-# because the Jenkins Git-plugin default branch is 'no-branch'
-# git checkout master
-
-################################
-# check for any changes in reader.js, and bump version if so
-################################
-
-npm run tags
-
 # build and run unit tests (creates 'dist' folder)
 echo "building..."
 grunt ci-init ci-build
--- vagrant-conf/scripts/ci-publish.sh
+++ vagrant-conf/scripts/ci-publish.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# build
-vagrant ssh-config | ssh -F /dev/stdin default 'cd /vagrant;npm run push && npm run pages;'
+npm run push && npm run pages
