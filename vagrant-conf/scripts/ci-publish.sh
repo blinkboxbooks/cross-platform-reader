c97c5d639165321a22b679d732308272607b9e40
Vilmos Ioo <vilmosi@blinkbox.com>
2014-07-01 16:11:09 +0100

    Added ci publish

--- vagrant-conf/scripts/ci-publish.sh
+++ vagrant-conf/scripts/ci-publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-npm run push && npm run pages
+# check for any changes in reader.js, and bump version if so
+vagrant ssh-config | ssh -F /dev/stdin default 'cd /vagrant;npm run push && npm run pages;'
