d234f9191b03169f89fc630471f562e13eb5e8f7
Vilmos Ioo <vilmosi@blinkbox.com>
2014-07-01 15:07:35 +0100

    Added comments

--- config/build.sh
+++ config/build.sh
@@ -1,15 +1,5 @@
 #! /bin/bash
 
-################################
-# Build the cross platform reader
-################################
-
-# install dependencies
-echo "installing dependencies..."
-
-npm install --quiet
-bower install
-
 # build and run unit tests (creates 'dist' folder)
 echo "building..."
 grunt ci-init ci-build
--- vagrant-conf/scripts/init.sh
+++ vagrant-conf/scripts/init.sh
@@ -6,5 +6,7 @@ cd /vagrant
 npm install && bower install
 
 # start  a selenium server to run in the background for e2e tests
+# we must replace stdin and out to allow the command to exit, it will print results in a file instead
+# http://stackoverflow.com/questions/8208033/what-does-dev-null-dev-null-at-the-end-of-a-command-do
 nohup node_modules/protractor/bin/webdriver-manager start 0<&- &>/dev/null &
 
