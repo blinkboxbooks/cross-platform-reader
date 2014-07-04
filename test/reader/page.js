32a7e185c576a3b0dbffa33be9831889bc0e9955
Vilmos Ioo <vilmosi@blinkbox.com>
2014-07-04 14:25:36 +0100

    Waiting for reader to load when loading a page

--- test/reader/page.js
+++ test/reader/page.js
@@ -25,6 +25,10 @@ var Page = function(){
 	this.load = function(isbn, env, publisherStyles){
 		browser.get(this.path + (isbn || '9780007441235') + '?env=' + (typeof env === 'undefined' ? 2 : env) + '&publisherStyles='+(!!publisherStyles ? 'true' : 'false')+'&transitionDuration=0');
 		browser.waitForAngular();
+		// wait at maximum 2 seconds for the reader to load the content (which means waiting for a status updated from the reader).
+		return browser.wait(function() {
+			return status.isPresent();
+		}, 2000);
 	};
 
 	this.next = function(){
