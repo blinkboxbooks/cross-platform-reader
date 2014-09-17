3b0a8fb300b2c6f63cde3af715f0d6b8cc013278
Vilmos Ioo <vilmosi@blinkbox.com>
2014-09-17 14:10:28 +0100

    Added setHighlight button

--- app/demo/scripts/script.js
+++ app/demo/scripts/script.js
@@ -169,12 +169,9 @@ angular.module('app', ['ngRoute'])
 				READER.next();
 				$scope.book.hasPrevious = true;
 			},
-			cfi: function(){
-				try{
-					_log(decodeURIComponent(READER.getCFI()));
-				} catch(e){
-					_log(e);
-				}
+			highlight: function(){
+				$('[data-test="status"]').removeAttr('data-test');
+				READER.setHighlight();
 			},
 			bookmark: function(){
 				$('[data-test="status"]').removeAttr('data-test');
@@ -245,7 +242,9 @@ angular.module('app', ['ngRoute'])
 		$scope.$on('keydown:37', function(){
 			$timeout($scope.handlers.prev);
 		});
-
+		$scope.$on('keydown:72', function(){
+			$timeout($scope.handlers.highlight);
+		});
 	})
 	// Service to access to the API of Catalogue
 	.factory('Book', function($http, $q) {
