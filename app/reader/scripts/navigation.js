0fe16027790550c7ae5ecc235a0bf4683c91fb08
Sebastian Tschan <sebastiant@blinkbox.com>
2014-11-07 14:21:37 +0000

    Reverted back to CSS transforms for navigation.

--- app/reader/scripts/navigation.js
+++ app/reader/scripts/navigation.js
@@ -69,10 +69,7 @@ var Reader = (function (r) {
 
 	r.getReaderLeftPosition = function () {
 		// Transform value is matrix(a, c, b, d, tx, ty)
-		//return parseInt(r.$reader.css('transform').split(',')[4], 10) || 0;
-		// Using position left instead of translateX transform, as the latter causes rendering issues on iOS,
-		// see http://jira.blinkbox.local/jira/browse/CR-419
-		return parseInt(r.$reader.css('left')) || 0;
+		return parseInt(r.$reader.css('transform').split(',')[4], 10) || 0;
 	};
 
 	r.setReaderLeftPosition = function (pos, duration) {
@@ -82,10 +79,7 @@ var Reader = (function (r) {
 		// for the next transitionend event in some cases (e.g. the transition unit tests).
 		r.$reader.css({
 			'transition-duration': '0s',
-			//transform: 'translateX(' + r.getReaderLeftPosition() + 'px)'
-			// Using position left instead of translateX transform, as the latter causes rendering issues on iOS,
-			// see http://jira.blinkbox.local/jira/browse/CR-419
-			left: r.getReaderLeftPosition() + 'px'
+			transform: 'translateX(' + r.getReaderLeftPosition() + 'px)'
 		}).trigger(r.support.transitionend);
 		if (duration) {
 			r.$reader.one(r.support.transitionend, defer.resolve);
@@ -94,10 +88,7 @@ var Reader = (function (r) {
 		}
 		r.$reader.css({
 			'transition-duration': (duration || 0) + 's',
-			//transform: 'translateX(' + pos + 'px)'
-			// Using position left instead of translateX transform, as the latter causes rendering issues on iOS,
-			// see http://jira.blinkbox.local/jira/browse/CR-419
-			left: pos + 'px'
+			transform: 'translateX(' + pos + 'px)'
 		});
 		return defer.promise();
 	};
