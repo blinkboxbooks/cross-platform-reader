383820e71d887dad853d95405c5890af40418fa2
Sebastian Tschan <sebastiant@blinkbox.com>
2014-11-17 17:22:34 +0000

    Allow tagging of development (-beta) builds.
    This commit splits the ci-publish task into a ci-tag and ci-publish tag.
    The first one performs a git tag on the master or development branch
    (depending on the $BUILD_SUFFIX).
    The latter publishes to gh-pages.

--- vagrant-conf/scripts/ci-publish.sh
+++ vagrant-conf/scripts/ci-publish.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+#! /bin/bash
 
-./config/push.sh
-./config/pages.sh
+echo "Updating Github Pages..."
+
+READER_VERSION='v'$(grep version package.json | awk -F\" '{print $(NF-1)}')
+READER_VERSION=$READER_VERSION-$BUILD_NUMBER
+
+git remote update
+git fetch
+git checkout -B gh-pages origin/gh-pages
+rm -r latest
+git rm -r latest
+cp -r dist latest
+cp -r dist $READER_VERSION
+git add latest
+git add $READER_VERSION
+git commit -m "Added build $READER_VERSION to gh-pages"
+git push origin gh-pages
+git checkout master
